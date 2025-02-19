2101f919b48407f85be4ddc52ba2c199b3c7d23b
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2025-02-19 14:48:34 +0530

    Add files via upload

---
+++ week-16-common-monorepo-framework-in-nodejs/16.1-mono&turborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import Admin from "@repo/ui/admin";
+
+export default function admin(){
+    return (
+        <div>
+            <Admin appName = "web slash admin from web"/>
+        </div>
+    )
+}
---
+++ week-16-common-monorepo-framework-in-nodejs/16.1-mono&turborepo/packages/ui/src/admin.tsx
@@ -0,0 +1,7 @@
+export default function Admin({appName}:any){
+    return (
+        <div>
+            Hii this is {appName} page
+        </div>
+    )
+}
